b981a481ff5b31ebe9cb9c53dc31ede77db924ad
Bruno Oliveira <brunooliveira812@gmail.com>
2019-08-11 00:53:28 -0300

    appsettings port configuration

--- CadastroDinamico.Web/Program.cs
+++ CadastroDinamico.Web/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CadastroDinamico.Web.Extension;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Globalization;
 using System.Net;
 
@@ -8,24 +10,43 @@ namespace CadastroDinamico.Web
 {
     public class Program
     {
+        private static IConfigurationRoot configurationRoot;
+
         public static void Main(string[] args)
         {
             ConfigurarAplicacao();
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel((hostingContext, opts) =>
+        public static IConfigurationRoot CreateConfiguration(string path) =>
+            new ConfigurationBuilder().AddJsonFile(path, false, true).Build();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            int httpPort = GetValue<int>("httpPort");
+            int httpsPort = GetValue<int>("httpsPort");
+
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel(opts =>
                 {
-                    opts.Listen(IPAddress.Loopback, hostingContext.RetornarPortaAmbiente());
+                    opts.Listen(IPAddress.Loopback, httpPort);
+                    opts.Listen(IPAddress.Loopback, httpsPort, config =>
+                    {
+                        config.UseHttps();
+                    });
                 })
                 .UseStartup<Startup>();
+        }
 
         private static void ConfigurarAplicacao()
         {
-            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR", false);
-            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR", false);
+            string environmentFileName = $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json";
+            configurationRoot = CreateConfiguration(System.IO.File.Exists(environmentFileName) ? environmentFileName : "appsettings.json");
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(GetValue<string>("cultureInfo"), false);
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(GetValue<string>("cultureInfo"), false);
         }
+
+        private static TType GetValue<TType>(string key) => configurationRoot.GetValue<TType>($"hosting:{key}");
     }
 }
